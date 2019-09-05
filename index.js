18a27fcb15a202598e33911877d81043aaa2d032
Chintan Kukadiya <43231930+ckukadiya@users.noreply.github.com>
2019-09-05 17:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ server.post('/get-movie-details', (req, res) => {
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
-                speech: dataToSend,
+                speech: 'Chintan Nu Bots',
                 displayText: dataToSend,
                 source: 'get-movie-details'
             });
